80a2b06a59b3610fd1b3f4625375f10a4d99555c
Scott Mabin <MabezDev@gmail.com>
2018-10-17 00:45:24 +0100

    Removed examples. Added some more comments/code fragments from monotron

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,40 @@ fn main() -> ! {
     // the binary will be sent as two ascii encoded bytes i.e 1 to F, anything outside this range will finish the loading
     // making a range of 0x01 to 0xFF 
 
+    // convert the first 4 bytes into a ffi function pointer
+    // let addr = ((APPLICATION_RAM[3] as u32) << 24)
+    //         | ((APPLICATION_RAM[2] as u32) << 16)
+    //         | ((APPLICATION_RAM[1] as u32) << 8)
+    //         | ((APPLICATION_RAM[0] as u32) << 0);
+    // let ptr = addr as *const ();
+    // let code: extern "C" fn(*const Table) -> u32 = ::core::mem::transmute(ptr);
+    // excute the function
+    // let result = code(&t);
+
     loop {
         // your code goes here
     }
 }
+
+//TODO: think about ABI required, example below
+// #[repr(C)]
+// struct Table {
+//     context: *mut Context,
+//     putchar: extern "C" fn(*mut Context, u8) -> i32,
+//     puts: extern "C" fn(*mut Context, *const u8) -> i32,
+//     readc: extern "C" fn(*mut Context) -> i32,
+//     wfvbi: extern "C" fn(*mut Context),
+//     kbhit: extern "C" fn(*mut Context) -> i32,
+//     move_cursor: extern "C" fn(*mut Context, u8, u8),
+//     play: extern "C" fn (*mut Context, u32, u8, u8, u8) -> i32,
+// }
+// let t = Table {
+//     context: context as *mut Context,
+//     putchar: api::putchar,
+//     puts: api::puts,
+//     readc: api::readc,
+//     wfvbi: api::wfvbi,
+//     kbhit: api::kbhit,
+//     move_cursor: api::move_cursor,
+//     play: api::play,
+// };
