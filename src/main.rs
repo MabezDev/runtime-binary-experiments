7f5cff36f696b1cd6d7d83747f1221abd59bb105
Scott Mabin <MabezDev@gmail.com>
2018-10-18 01:39:02 +0100

    Working example of dynamic loading and excution for a demo SDK, can reload and excute different apps at run time

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,14 @@ use core::fmt::Write;
 
 #[link_section = ".app_section.data"]
 static mut APPLICATION_RAM: [u8; 32 * 1024] = [0u8; 32 * 1024];
+static mut WRITE_BUFFER: [u8; 128] = [0u8; 128];
 
 #[exception]
 fn HardFault(ef: &ExceptionFrame) -> ! {
     panic!("{:#?}", ef);
 }
+
+pub struct Context {}
 
 #[entry]
 fn main() -> ! {
@@ -28,7 +31,8 @@ fn main() -> ! {
     let app_addr = unsafe { &APPLICATION_RAM as *const _ } as usize;
     
     let p = stm32l4x2::Peripherals::take().unwrap();
-
+    let mut context = Context {};
+    let context = &mut context;
     let mut flash = p.FLASH.constrain();
     let mut rcc = p.RCC.constrain();
     let mut gpioa = p.GPIOA.split(&mut rcc.ahb2);
@@ -41,132 +45,143 @@ fn main() -> ! {
     let serial = Serial::usart1(p.USART1, (tx, rx), 9_600.bps(), clocks, &mut rcc.apb2);
     let (mut tx, mut rx) = serial.split();
 
-    for _b in b"\rload\r" {
-        block!(rx.read()).unwrap();
-    }
+    loop {
 
-    for b in b"READY" {
-        block!(tx.write(*b)).ok();
-    }
+        for _b in b"\rload\r" {
+            block!(rx.read()).unwrap();
+        }
 
-    let mut i = 0;
-    let max_bytes = unsafe { APPLICATION_RAM.len() };
-    const ACK_EVERY: usize = 4;
-    let mut ack_count = 0;
-    while i < max_bytes {
-        let ch = loop {
-            match block!(rx.read()) {
-                Ok(x) => break x,
-                _ => {}
+        for b in b"READY" {
+            block!(tx.write(*b)).ok();
+        }
+
+        let mut i = 0;
+        let max_bytes = unsafe { APPLICATION_RAM.len() };
+        const ACK_EVERY: usize = 4;
+        let mut ack_count = 0;
+        while i < max_bytes {
+            let ch = loop {
+                match block!(rx.read()) {
+                    Ok(x) => break x,
+                    _ => {}
+                }
+            };
+            let mut byte = match ch {
+                b'0' => 0x00,
+                b'1' => 0x10,
+                b'2' => 0x20,
+                b'3' => 0x30,
+                b'4' => 0x40,
+                b'5' => 0x50,
+                b'6' => 0x60,
+                b'7' => 0x70,
+                b'8' => 0x80,
+                b'9' => 0x90,
+                b'A' => 0xA0,
+                b'B' => 0xB0,
+                b'C' => 0xC0,
+                b'D' => 0xD0,
+                b'E' => 0xE0,
+                b'F' => 0xF0,
+                b'a' => 0xA0,
+                b'b' => 0xB0,
+                b'c' => 0xC0,
+                b'd' => 0xD0,
+                b'e' => 0xE0,
+                b'f' => 0xF0,
+                _ => break,
+            };
+            let ch = loop {
+                match block!(rx.read()) {
+                    Ok(x) => break x,
+                    _ => {}
+                }
+            };
+            byte |= match ch {
+                b'0' => 0x00,
+                b'1' => 0x01,
+                b'2' => 0x02,
+                b'3' => 0x03,
+                b'4' => 0x04,
+                b'5' => 0x05,
+                b'6' => 0x06,
+                b'7' => 0x07,
+                b'8' => 0x08,
+                b'9' => 0x09,
+                b'A' => 0x0A,
+                b'B' => 0x0B,
+                b'C' => 0x0C,
+                b'D' => 0x0D,
+                b'E' => 0x0E,
+                b'F' => 0x0F,
+                b'a' => 0x0A,
+                b'b' => 0x0B,
+                b'c' => 0x0C,
+                b'd' => 0x0D,
+                b'e' => 0x0E,
+                b'f' => 0x0F,
+                _ => break,
+            };
+            unsafe {
+                APPLICATION_RAM[i] = byte;
+                ack_count += 1;
+                if ack_count >= ACK_EVERY {
+                    let _ = block!(tx.write(b'X')).ok();
+                    ack_count = 0;
+                }
             }
-        };
-        let mut byte = match ch {
-            b'0' => 0x00,
-            b'1' => 0x10,
-            b'2' => 0x20,
-            b'3' => 0x30,
-            b'4' => 0x40,
-            b'5' => 0x50,
-            b'6' => 0x60,
-            b'7' => 0x70,
-            b'8' => 0x80,
-            b'9' => 0x90,
-            b'A' => 0xA0,
-            b'B' => 0xB0,
-            b'C' => 0xC0,
-            b'D' => 0xD0,
-            b'E' => 0xE0,
-            b'F' => 0xF0,
-            b'a' => 0xA0,
-            b'b' => 0xB0,
-            b'c' => 0xC0,
-            b'd' => 0xD0,
-            b'e' => 0xE0,
-            b'f' => 0xF0,
-            _ => break,
-        };
-        let ch = loop {
-            match block!(rx.read()) {
-                Ok(x) => break x,
-                _ => {}
-            }
-        };
-        byte |= match ch {
-            b'0' => 0x00,
-            b'1' => 0x01,
-            b'2' => 0x02,
-            b'3' => 0x03,
-            b'4' => 0x04,
-            b'5' => 0x05,
-            b'6' => 0x06,
-            b'7' => 0x07,
-            b'8' => 0x08,
-            b'9' => 0x09,
-            b'A' => 0x0A,
-            b'B' => 0x0B,
-            b'C' => 0x0C,
-            b'D' => 0x0D,
-            b'E' => 0x0E,
-            b'F' => 0x0F,
-            b'a' => 0x0A,
-            b'b' => 0x0B,
-            b'c' => 0x0C,
-            b'd' => 0x0D,
-            b'e' => 0x0E,
-            b'f' => 0x0F,
-            _ => break,
-        };
+            i = i + 1;
+        }
+
+        // read in the binary
+        // the binary will be sent as two ascii encoded bytes i.e 1 to F, anything outside this range will finish the loading
+        // making a range of 0x01 to 0xFF 
+
+        // convert the first 4 bytes into a ffi function pointer
         unsafe {
-            APPLICATION_RAM[i] = byte;
-            ack_count += 1;
-            if ack_count >= ACK_EVERY {
-                let _ = block!(tx.write(b'X')).ok();
-                ack_count = 0;
+            let addr = ((APPLICATION_RAM[3] as u32) << 24)
+                    | ((APPLICATION_RAM[2] as u32) << 16)
+                    | ((APPLICATION_RAM[1] as u32) << 8)
+                    | ((APPLICATION_RAM[0] as u32) << 0);
+            let ptr = addr as *const ();
+
+            // struct callbacks_t {
+            //     void* p_context;
+            //     int32_t(*puts)(void* p_context, const char*);
+            // };
+            writeln!(hstdout, "Loaded {} bytes into buffer at {:08X}. Will execute at {:08X}.", i, app_addr, addr).unwrap();
+
+            #[repr(C)]
+            struct Table {
+                context: *mut Context,
+                puts: extern "C" fn(*mut Context, *const u8) -> i32,
+            };
+            let t = Table {
+                context: context as *mut Context,
+                puts: puts,
+            };
+            let code: extern "C" fn(*const Table) -> u32 = ::core::mem::transmute(ptr);
+            // excute the function
+            let result = code(&t);
+
+            writeln!(hstdout, "Result of execution {}", result);
+            for i in 0..WRITE_BUFFER.len() {
+                if WRITE_BUFFER[i] != 0 {
+                    write!(hstdout, "{}", WRITE_BUFFER[i] as char);
+                }
             }
         }
-        i = i + 1;
-    }
-
-    // read in the binary
-    // the binary will be sent as two ascii encoded bytes i.e 1 to F, anything outside this range will finish the loading
-    // making a range of 0x01 to 0xFF 
-
-    // convert the first 4 bytes into a ffi function pointer
-    unsafe {
-        let addr = ((APPLICATION_RAM[3] as u32) << 24)
-                | ((APPLICATION_RAM[2] as u32) << 16)
-                | ((APPLICATION_RAM[1] as u32) << 8)
-                | ((APPLICATION_RAM[0] as u32) << 0);
-        let _ptr = addr as *const ();
-        // let code: extern "C" fn(*const Table) -> u32 = ::core::mem::transmute(ptr);
-        // excute the function
-        // let result = code(&t);
-        writeln!(hstdout, "Loaded {} bytes into buffer at {:08X}. Will execute at {:08X}.", i, app_addr, addr).unwrap();
-    }
-    loop {
-        // your code goes here
     }
 }
 
-//TODO: think about ABI required, example below
-// #[repr(C)]
-// struct Table {
-//     context: *mut Context,
-//     putchar: extern "C" fn(*mut Context, u8) -> i32,
-//     puts: extern "C" fn(*mut Context, *const u8) -> i32,
-//     readc: extern "C" fn(*mut Context) -> i32,
-//     wfvbi: extern "C" fn(*mut Context),
-//     kbhit: extern "C" fn(*mut Context) -> i32,
-//     move_cursor: extern "C" fn(*mut Context, u8, u8),
-//     play: extern "C" fn (*mut Context, u32, u8, u8, u8) -> i32,
-// }
-// let t = Table {
-//     context: context as *mut Context,
-//     putchar: api::putchar,
-//     puts: api::puts,
-//     readc: api::readc,
-//     wfvbi: api::wfvbi,
-//     kbhit: api::kbhit,
-//     move_cursor: api::move_cursor,
-//     play: api::play,
-// };
+pub(crate) extern "C" fn puts(_raw_ctx: *mut Context, s: *const u8) -> i32 {
+    let mut i = 0;
+    unsafe {
+        while *s.offset(i) != 0 {
+            let ch: u8 = *s.offset(i);
+            WRITE_BUFFER[i as usize] = ch;
+            i += 1;
+        }
+    }
+    0
+}
